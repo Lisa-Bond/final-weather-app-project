a39e1d0a6d7aa871a22e16a4be2ae7f04da4eddb
Lisa Bondarenko <lisa.bond.github@gmail.com>
2022-08-29 22:38:44 +0300

    Added the Github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Student Project React Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This app was coded by{" "}
+        <a href="https://www.facebook.com/lisabondarenko" target="_blank">
+          Lisa Bondarenko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Lisa-Bond/final-weather-app-project"
+          target="_blank"
+        >
+          opened-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
