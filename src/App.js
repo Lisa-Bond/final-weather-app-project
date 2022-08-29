641e5ced87be2007bc8202e1762076d4200f43a9
Lisa Bondarenko <lisa.bond.github@gmail.com>
2022-08-29 23:30:51 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This app was coded by{" "}
-        <a href="https://www.facebook.com/lisabondarenko" target="_blank">
-          Lisa Bondarenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Lisa-Bond/final-weather-app-project"
-          target="_blank"
-        >
-          opened-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This app was coded by{" "}
+          <a href="https://www.facebook.com/lisabondarenko" target="_blank">
+            Lisa Bondarenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Lisa-Bond/final-weather-app-project"
+            target="_blank"
+          >
+            opened-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
